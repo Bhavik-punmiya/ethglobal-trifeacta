ae81e597499454f3a0a49efece1df100bfb37b91
Bhavik-punmiya <Bhavikpunmiya@gmail.com>
2025-03-23 14:51:56 +0530

    firebase config

--- client/lib/firebase.js
+++ client/lib/firebase.js
@@ -1,21 +1,19 @@
-// src/lib/firebase.js
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDyCXKVWY6n9SuPLCLQkDG1P7wVuT8MdK8",
-	authDomain: "decentralizedkaggle.firebaseapp.com",
-	projectId: "decentralizedkaggle",
-	storageBucket: "decentralizedkaggle.firebasestorage.app",
-	messagingSenderId: "430176236491",
-	appId: "1:430176236491:web:fdd64a879529c1ff2cfc4e",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app =
-	getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
